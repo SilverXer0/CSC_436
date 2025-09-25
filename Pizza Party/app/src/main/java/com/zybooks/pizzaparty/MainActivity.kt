8f3f467c119fd036b47ca0cc522c65461360dff4
Sharan Krishna <87695707+SilverXer0@users.noreply.github.com>
2025-09-25 16:55:37 -0700

    hoisted numberField's state

--- Pizza Party/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ Pizza Party/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -50,6 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PizzaPartyScreen(modifier: Modifier = Modifier) {
     var totalPizzas by remember { mutableIntStateOf(0) }
+    var numPeopleInput by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier.padding(10.dp)
@@ -61,6 +62,8 @@ fun PizzaPartyScreen(modifier: Modifier = Modifier) {
         )
         NumberField(
             labelText = "Number of people?",
+            textInput = numPeopleInput,
+            onValueChange = { numPeopleInput = it },
             modifier = modifier.padding(bottom = 16.dp).fillMaxWidth()
         )
         RadioGroup(
@@ -88,13 +91,13 @@ fun PizzaPartyScreen(modifier: Modifier = Modifier) {
 @Composable
 fun NumberField(
     labelText: String,
+    textInput: String,
+    onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var textInput by remember { mutableStateOf("") }
-
     TextField(
         value = textInput,
-        onValueChange = { textInput = it },
+        onValueChange = onValueChange,
         label = { Text(labelText) },
         singleLine = true,
         keyboardOptions = KeyboardOptions(
